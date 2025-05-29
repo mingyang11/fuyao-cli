885f9f920cbdb4191223c3bd13b7d023b8302618
mingyang11 <2578349261@qq.com>
2025-05-29 22:28:04 +0800

    feat: 完善本地模版瞎子啊

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -15,12 +15,14 @@ const create = (program: Command) => {
             .description('创建项目')
             .helpOption('-h, --help', '显示帮助信息')
             .action(async (projectName?: string, options?: CommandOptions) => {
-                const { template } = options;
+                const { template: t } = options as { template: string };
+
+                let template = t;
                 /**
                  * 项目名称处理
                  */
                 if (!projectName) {
-                    const result = await prompts({
+                    const projectResult = await prompts({
                         type: 'text',
                         name: 'projectName',
                         message: '请输入项目名称',
@@ -32,9 +34,9 @@ const create = (program: Command) => {
                     });
 
                     // 取消创建
-                    if (!result.projectName) return;
+                    if (!projectResult.projectName) return;
                     // 设置项目名称为交互输入的值
-                    projectName = result.projectName;
+                    projectName = projectResult.projectName;
                 }
 
                 // 如果项目名称仍然为空，则提示错误（一般不会走到这里）
@@ -43,13 +45,52 @@ const create = (program: Command) => {
                     return;
                 }
 
-                // 如果模板不存在，则推交互，日志提示错误
-                if (!templateList.includes(template)) {
+                /**
+                 * 输入了模版，但是模版不存在的情况
+                 */
+                if (!templateList.includes(template) && template) {
                     console.log('模板不存在');
                     return;
                 }
 
-                console.log('create', projectName, options);
+                /**
+                 * 没有输入模版的情况
+                 */
+                if (!template) {
+                    const templateResult = await prompts({
+                        type: 'select',
+                        name: 'template',
+                        message: '请选择模板',
+                        choices: templateList.map(item => ({
+                            title: item,
+                            value: item
+                        }))
+                    });
+                    if (!templateResult.template) {
+                        console.log('取消创建');
+                        return;
+                    }
+
+                    template = templateResult.template;
+                }
+
+                /**
+                 * 选择使用本地模版还是远程模版
+                 */
+                const originResult = await prompts({
+                    type: 'confirm',
+                    name: 'origin',
+                    message: '是否使用远程模版',
+                    initial: false
+                });
+                // 使用远程模版，从git上拉模版代码
+                if (originResult.origin) {
+                    console.log('暂不支持远程模版');
+                    return;
+                } else {
+                    // 使用本地模版
+                    loadTemplate(template, projectName);
+                }
             })
     );
 };
--- src/utils/loadTemplate.ts
+++ src/utils/loadTemplate.ts
@@ -1,6 +1,12 @@
-import { copy } from 'fs-extra'
-import path from 'path'
+import { copy } from 'fs-extra';
+import path from 'path';
 
-export const loadTemplate = async (template: string) => {
-    await copy(path.join(__dirname, `../templates/${template}`), process.cwd())
-}
+/**
+ * 加载本地模版
+ * @param template 模版名称
+ * @param projectName 项目名称
+ */
+export const loadTemplate = async (template: string, projectName?: string) => {
+    await copy(path.join(__dirname, `../templates/template-${template}`), path.join(process.cwd(), projectName));
+    console.log(`${projectName} 创建成功，请进入项目目录：cd ${projectName}`);
+};
